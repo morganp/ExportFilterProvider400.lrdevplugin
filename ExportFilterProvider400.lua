203aa772b9930036771d20869d79ee6907743aae
Morgan Prior <morgan@amaras-tech.co.uk>
2014-04-08 12:31:08 +0100

    Update ExportFilterProvider400.lua
    Added postProcessRenderedPhotos for Export Filters, updateExportSettings is for Export Services

--- ExportFilterProvider400.lua
+++ ExportFilterProvider400.lua
@@ -34,19 +34,52 @@ end
 
 --------------------------------------------------------------------------------
 -- Example on updating export settings
+-- This preset expot settings for a Export Service
 local function updateExportSettings( exportSettings ) 
   exportSettings.LR_size_maxHeight = 400
   exportSettings.LR_size_maxWidth  = 400
   exportSettings.LR_size_doConstrain = true
 end
 
-
+-- Override export settings with Export Filter
+-- Page 49 to 51 of the SDK guide
+-- http://wwwimages.adobe.com/www.adobe.com/content/dam/Adobe/en/devnet/photoshoplightroom/pdfs/lr5/lightroom-sdk-guide.pdf
+local function postProcessRenderedPhotos(  functionContext, filterContext )
+  -- Optional: If you want to change the render settings for each photo
+  -- before Lightroom renders it
+  local renditionOptions = {
+    filterSettings = function( renditionToSatisfy, exportSettings )
+      exportSettings.LR_size_maxHeight = 400
+      exportSettings.LR_size_maxWidth  = 400
+      exportSettings.LR_size_doConstrain = true
+      return os.tmpname()
+    end,
+  }
+  for sourceRendition, renditionToSatisfy in filterContext:renditions( 
+    renditionOptions ) do
+    -- Wait for the upstream task to finish its work on this photo. 
+    local success, pathOrMessage = sourceRendition:waitForRender()
+    --if success then 
+    --  -- Now that the photo is completed and available to this filter,
+    --  -- you can do your work on the photo here.
+    --  -- It would look somethinglike this: 
+    --  -- local status = LrTasks.execute( 'mytool "' .. pathOrMessage .. '"' )
+    --  local status = 1
+    --  -- use something like this to signal a failure for this rendition only: 
+    --  -- (Replace "error message" with a user-readable string explaining why
+    --  -- the photo failed to render.)
+    --  if status ~= (desired status) then
+    --    renditionToSatisfy:renditionIsDone( false, "error message" )
+    --  end
+  end
+end
 
 --------------------------------------------------------------------------------
 
 return {
-sectionForFilterInDialog = sectionForFilterInDialog,
-updateExportSettings     = updateExportSettings    , --Does this works
+sectionForFilterInDialog  = sectionForFilterInDialog,
+updateExportSettings      = updateExportSettings    , --Does this works
+postProcessRenderedPhotos = postProcessRenderedPhotos,
 }
 
 --ExportFilterProvider
